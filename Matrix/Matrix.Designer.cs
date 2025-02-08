99f8b7836fc05f3207e270e0976a2e0e21ef8933
Lazar Djokovic <djokoviclazar315@gmail.com>
2025-02-09 00:41:20 +0100

    Adding MatrixNxM grid app

---
+++ Matrix/Matrix.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Januar_2025
+{
+    partial class Zadatak1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtn = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtm = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.broj = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.MatricaM = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(570, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Povratak";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(710, 46);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(321, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Za broj redova, n i kolona m, vazi 4<=n,m<=7";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(255, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Uneti dimenziju matrice M(nxm), n=";
+            // 
+            // txtn
+            // 
+            this.txtn.Location = new System.Drawing.Point(278, 66);
+            this.txtn.Name = "txtn";
+            this.txtn.Size = new System.Drawing.Size(55, 26);
+            this.txtn.TabIndex = 3;
+            this.txtn.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(339, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "m=";
+            // 
+            // txtm
+            // 
+            this.txtm.Location = new System.Drawing.Point(376, 66);
+            this.txtm.Name = "txtm";
+            this.txtm.Size = new System.Drawing.Size(55, 26);
+            this.txtm.TabIndex = 5;
+            this.txtm.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(462, 59);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(138, 40);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Formiraj";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Uneti broj:";
+            this.label4.Visible = false;
+            // 
+            // broj
+            // 
+            this.broj.Location = new System.Drawing.Point(104, 110);
+            this.broj.Name = "broj";
+            this.broj.Size = new System.Drawing.Size(65, 26);
+            this.broj.TabIndex = 8;
+            this.broj.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.broj.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(195, 107);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(138, 29);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Dodaj broj";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(17, 145);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Matrica M";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(20, 471);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 20);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Matrica M";
+            this.label6.Visible = false;
+            // 
+            // MatricaM
+            // 
+            this.MatricaM.Location = new System.Drawing.Point(23, 505);
+            this.MatricaM.Multiline = true;
+            this.MatricaM.Name = "MatricaM";
+            this.MatricaM.Size = new System.Drawing.Size(384, 142);
+            this.MatricaM.TabIndex = 13;
+            this.MatricaM.Visible = false;
+            // 
+            // Matrix
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(710, 659);
+            this.ControlBox = false;
+            this.Controls.Add(this.MatricaM);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.broj);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtm);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.KeyPreview = true;
+            this.Name = "Matrix";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Matrix";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtm;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox broj;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox MatricaM;
+    }
+}
---
+++ Matrix/Matrix.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Matrix
+{
+    public partial class Matrix : Form
+    {
+        static int red = 0;
+        static int kol = 0;
+        static int[,] mM;
+        static int n;
+        static int m;
+        private ListView Matrica;
+        public Matrix()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();   
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string nn = txtn.Text.Trim();
+            string mm = txtm.Text.Trim();
+            if (string.IsNullOrWhiteSpace(nn) || string.IsNullOrWhiteSpace(mm))
+            {
+                MessageBox.Show("Enter n, m between 4 & 7", "Parse Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                n = Int32.Parse(nn);
+                m = Int32.Parse(mm);
+                if(n<4 || n>7 || m<4 || m>7)
+                {
+                    MessageBox.Show("n & m bust be between 4 & 7", "Parse Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    mM = new int[n, m];
+                    Matrica = new ListView
+                    {
+                        View = View.Details,
+                        FullRowSelect = true,
+                        GridLines = true,
+                        Location = new Point(21,168),
+                        //m column, width per column 70
+                        Size= new Size((m+1)*70+10,184)
+                    };
+                    //adding ListView object in form
+                    this.Controls.Add( Matrica );
+                    for (int i = 0; i <= m; i++)
+                    {
+                        ColumnHeader kolona = new ColumnHeader
+                        {
+                            Text = i < m ? $"K {i + 1}" : "SR.V",//title
+                            Width = 70
+                        };
+                        Matrica.Columns.Add( kolona );
+                    }
+                    label4.Visible= true;
+                    broj.Visible= true;
+                    button3.Visible = true;
+                    label6.Visible = true;
+                    MatricaM.Visible= true;
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string sBroj=broj.Text.Trim();
+            if (string.IsNullOrWhiteSpace(sBroj))
+                MessageBox.Show("Please enter some number!!!", "Parse Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else
+            { 
+                int iBroj=Int32.Parse(sBroj);
+                MatricaM.Text += sBroj + "\t";
+                mM[red, kol] = iBroj;
+                broj.Text = string.Empty;
+                kol++;
+                if(kol>=m)
+                {
+                    //show current row in Matrix object - ListView
+                    string[] redic=new string[m+1];
+                    int zbir = 0;
+                    for(int j = 0;j<m;j++) // this will aggregate avrage values per row, and increase it until the end
+                    {
+                        redic[j] = mM[red, j].ToString();
+                        zbir += mM[red, j];
+                    }
+                    float aRs = (float)zbir / m;
+                    redic[m]=aRs.ToString();
+                    ListViewItem lw = new ListViewItem(redic);
+                    Matrica.Items.Add(lw);
+                    red++;
+                    kol = 0;
+                }
+                if(kol>m)
+                {
+                    MatricaM.Text += "\r\n";
+                }
+                if(red>=n)
+                {
+                    string[] nizs=new string[n];
+                    label4.Visible = false;
+                    broj.Visible = false;
+                    button3.Visible = false;
+                    for(int j=0;j<m;j++)
+                    {
+                        int zbir = 0;
+                        for(int k=0;k<n;k++) // summing avrage per column.
+                                             // This will go in depth in 1 row until the botom is reached. 
+                                             // row remains the same. Step forward is only on column
+                        {
+                            zbir=zbir+mM[k,j]; 
+                        }
+                        float aRs=zbir / n;
+                        nizs[j]=aRs.ToString();
+                    }
+                    ListViewItem lw = new ListViewItem(nizs);
+                    Matrica.Items.Add(lw);
+                }
+            }
+        }
+    }
+}
