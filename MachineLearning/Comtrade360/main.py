f24ec5bc545fd8bb3ed72181740981df6bcef6ec
Lazar Djokovic <djokoviclazar315@gmail.com>
2025-02-19 21:55:40 +0100

    Fixing errors on main method

--- MachineLearning/Comtrade360/main.py
+++ MachineLearning/Comtrade360/main.py
@@ -1,19 +1,31 @@
 from fastapi import FastAPI
 from fastapi import Request, status
-from router import resource_get
-from exceptions import BadInputParsed, NoResultsException
+from router import anarchy_of_hierarchy, cohesion_among_categories
+from exceptions import BadInputParsed, NoResultsException, MissingInput, TimeOutException
 from fastapi.responses import  JSONResponse, HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
 from typing import Callable
 from loguru import logger
-from starlette.status import HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND
+from starlette.status import HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND, HTTP_405_METHOD_NOT_ALLOWED, HTTP_413_REQUEST_ENTITY_TOO_LARGE
 
 
 
 app = FastAPI(title="Analiza hijerarhije i slicnosti koncepata iz DBpedia baze")
-app.include_router(resource_get.router)
+app.include_router(anarchy_of_hierarchy.router)
+app.include_router(cohesion_among_categories.router)
 
-
+@app.exception_handler(MissingInput)
+async def missing_input_exception_handler(request: Request, exc: MissingInput):
+    return JSONResponse(
+        status_code=HTTP_405_METHOD_NOT_ALLOWED,
+        content={"detail": exc.detail},
+    )
+@app.exception_handler(TimeOutException)
+async def timeout_exception_handler(request: Request, exc: TimeOutException):
+    return JSONResponse(
+        status_code=HTTP_413_REQUEST_ENTITY_TOO_LARGE,
+        content={"detail": exc.detail},
+    )
 
 @app.exception_handler(BadInputParsed)
 async def bad_input_exception_handler(request: Request, exc: BadInputParsed):
