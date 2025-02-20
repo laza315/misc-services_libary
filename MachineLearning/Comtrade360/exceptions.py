935efaf08f8ba1f0e0c7a23c416aaa1a8f7a6db6
Lazar Djokovic <djokoviclazar315@gmail.com>
2025-02-20 07:23:27 +0100

    Adding logic for concept similarity in vector space model

--- MachineLearning/Comtrade360/exceptions.py
+++ MachineLearning/Comtrade360/exceptions.py
@@ -6,9 +6,20 @@ class BadInputParsed(HTTPException):
     def __init__(self, detail: str = "Invalid input. Only alphabetic strings are allowed"):
         super().__init__(status_code=400, detail=detail)
 
+class MissingInput(HTTPException):
+    """Input has not been provided"""
+    
+    def __init__(self, detail: str = "Invalid input. Please enter any sense string"):
+        super().__init__(status_code=405, detail=detail)
 
 class NoResultsException(HTTPException):
     """There were no results for searched criteria"""
     
-    def __init__(self, detail: str = "No results. Your searching criteria have no associated nodes"):
+    def __init__(self, detail: str = "No results. Your searching criteria doesn't exist on DBpedia!"):
         super().__init__(status_code=400, detail=detail)
+
+class TimeOutException(HTTPException):
+    """Time out exception occured. Site couldn't fetch the results in set time range"""
+    
+    def __init__(self, detail: str = "No results. Time limit exceeded"):
+        super().__init__(status_code=413, detail=detail)      
